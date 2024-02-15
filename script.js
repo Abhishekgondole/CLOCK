c2234066d645bbbdcf73ce960b9363e601768bab
ABHISHEK GONDOLE <154117151+Abhishekgondole@users.noreply.github.com>
2024-02-16 01:09:53 +0530

    js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ function displayTime() {
 	let mn = dt.getMinutes();
 	let sc = dt.getSeconds();
 	let tm = document.getElementById("tm");
-	tm.innerHTML = ("0"+hr+": "+mn+":"+sc);
+	tm.innerHTML = (hr+" : "+mn+" : "+sc); 
 }
 setInterval(displayTime,1000)
